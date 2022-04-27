15ada088c26318460138437a2626f5e1f237a812
Stanley Chong <73448184+stanley54073@users.noreply.github.com>
2022-04-26 23:58:37 -0700

    Derivative Implementation

--- calculator.cpp
+++ calculator.cpp
@@ -1,3 +1,19 @@
+/*
+Author Information:
+ Group Name: Group Project - Stanley Chong, Lauren Moon
+ Name:       Lauren Moon, Stanley Chong
+ Email:      lrn.moon1@csu.fullerton.edu, stanley54073@csu.fullerton.edu
+
+ This program is free software: you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free Software
+ Foundation, either version 3 of the License, or (at your option) any later
+ version.This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ details.You should have received a copy of the GNU General Public License
+ along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "calculator.h"
 #include "./ui_calculator.h"
 #include <QtMath>
